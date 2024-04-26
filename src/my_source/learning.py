75fc62c40e64460852563a5b98d39473c73a59e1
Jose Juan Montiel <josejuan.montiel@gmail.com>
2024-04-26 14:28:13 +0200

    Playing with pytest

---
+++ src/my_source/learning.py
@@ -0,0 +1,6 @@
+class Learning:
+    def func(x):
+        return x + 1
+    
+    def func2(x):
+        return x + 2
---
+++ tests/test_learning.py
@@ -0,0 +1,7 @@
+from my_source import learning
+
+class TestLearning:
+    def test_answer(x):
+        assert learning.Learning.func(3) == 4
+    def test_answer_2(x):
+        assert learning.Learning.func2(3) == 5        
